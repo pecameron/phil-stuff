445a0511ce5a32809bc4de2e125b9c0958d991d2
Phil Cameron <pcameron@redhat.com>
2020-02-18 17:06:05 -0500

    Skip must-gather, heap, prometheus.tar
    Signed-off-by: Phil Cameron <pcameron@redhat.com>

--- ocp/get-ci-artifacts.sh
+++ ocp/get-ci-artifacts.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+# set -x
 
 # When a ci failure occurs this script can download the artifacts 
 # for later offline analysis
 # this takes a while and is not always useful
 # analysis is left to the user (may integrate it with debugbootstrap)
 
-# set -x
 # $1 base directory to put results e.g., t12105
 # $2 base url e.g., https://gcsweb-ci.svc.ci.openshift.org/gcs/origin-ci-test/logs/release-openshift-origin-installer-e2e-aws-upgrade/12105/
 
 baseurl="https://gcsweb-ci.svc.ci.openshift.org"
 
-echo "basedir=${basedir}"
 basedir=${1}
+echo "basedir=${basedir}"
 mkdir -p ${basedir}
 cd ${basedir}
 
@@ -25,12 +25,22 @@ rm -f index.html
 wget ${2}
 
 list=$(grep -v "\.\." index.html | grep "><a href=" | sed 's;<div class="pure-u-2-5"><a href=";;' | sed 's/"><img//' | gawk '{ print $1 }')
-
 rm -f index.html
 
 for l in ${list}
 do
 	#echo PHIL $l
+	thisdir=$(basename $l)
+	#echo $thisdir
+	if [[ $thisdir == "must-gather" ]] ; then
+		continue
+	fi
+	if [[ $thisdir == "heap" ]] ; then
+		continue
+	fi
+	if [[ $thisdir == "prometheus.tar" ]] ; then
+		continue
+	fi
 	echo ${l} | grep "http" > /dev/null
 	if [[ $? -eq 0 ]] ; then
 		wget ${l}
