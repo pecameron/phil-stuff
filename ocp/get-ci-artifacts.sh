f21d0587af82b3190adfd6f5e1a2725d8c312c17
Phil Cameron <pcameron@redhat.com>
2020-02-20 10:25:14 -0500

    Continued work on analysis
    This is just a start, lots more needed.
    
    Signed-off-by: Phil Cameron <pcameron@redhat.com>

--- ocp/get-ci-artifacts.sh
+++ ocp/get-ci-artifacts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # set -x
 
+# Internal script called by get-ci-artifacts
+
 # When a ci failure occurs this script can download the artifacts 
 # for later offline analysis
 # this takes a while and is not always useful
@@ -45,6 +47,39 @@ do
 	if [[ $? -eq 0 ]] ; then
 		wget ${l}
 		echo will get $l
+		if [[ $thisdir == "masters-journal" ]] ; then
+			mv masters-journal masters-journal.gz
+			gunzip masters-journal.gz
+		fi
+		if [[ $thisdir == "workers-journal" ]] ; then
+			mv workers-journal workers-journal.gz
+			gunzip workers-journal.gz
+		fi
+		if [[ $thisdir == "prometheus-target-metadata.json" ]] ; then
+			mv prometheus-target-metadata.json prometheus-target-metadata.json.gz
+			gunzip prometheus-target-metadata.json.gz
+		fi
+		if [[ $thisdir == "statefulsets.json" ]] ; then
+			mv statefulsets.json statefulsets.json.gz
+			gunzip statefulsets.json.gz
+		fi
+		if [[ $thisdir == "replicasets.json" ]] ; then
+			mv rworkers-journal rworkers-journal.gz
+			gunzip rworkers-journal.gz
+		fi
+		if [[ $thisdir == "daemonsets.json" ]] ; then
+			mv daemonsets.json daemonsets.json.gz
+			gunzip daemonsets.json.gz
+		fi
+		if [[ $thisdir == "deployments.json" ]] ; then
+			mv deployments.json deployments.json.gz
+			gunzip deployments.json.gz
+		fi
+		if [[ $thisdir == "openapi.json" ]] ; then
+			mv openapi.json openapi.json.gz
+			gunzip openapi.json.gz
+		fi
+
 	else
 		# sub directory
 		dir=$(echo ${baseurl}${l} | sed "s;${2};;")
